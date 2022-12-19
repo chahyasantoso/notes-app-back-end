eb087d28f1625c2134ba5097c769772903d98ae2
Chahya Santoso <chahya.santoso@gmail.com>
2022-12-19 14:55:02 +0800

    data validation implemented

---
+++ src/exceptions/InvariantError.js
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-underscore-dangle */
+
+const ClientError = require('./ClientError');
+
+class InvariantError extends ClientError {
+  constructor(message) {
+    super(message);
+    this.name = 'InvariantError';
+  }
+}
+
+module.exports = InvariantError;
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/in-memory/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
   const notesService = new NotesService();
@@ -21,6 +22,7 @@ const init = async () => {
     plugin: notes,
     options: {
       service: notesService,
+      validator: NotesValidator,
     },
   });
 
